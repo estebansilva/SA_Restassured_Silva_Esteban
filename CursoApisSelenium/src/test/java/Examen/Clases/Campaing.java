f2276a51b15ff1d4ad26d40a295f2fc234cadcfe
DESKTOP-TO45NQG\esteb <estebansilva_utn@hotmail.com>
2022-02-22 22:04:15 -0300

    Resolución del examen de API

---
+++ CursoApisSelenium/src/test/java/Examen/Clases/Campaing.java
@@ -0,0 +1,18 @@
+package Examen.Clases;
+
+public class Campaing {
+
+    private boolean IsActive;
+    private String Name;
+    private String Status;
+
+
+    //Constructor
+
+    public Campaing (boolean aFlagIsActive, String aName, String aStatus){
+        this.IsActive = aFlagIsActive;
+        this.Name = aName;
+        this.Status = aStatus;
+
+    }
+}
